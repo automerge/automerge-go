2c3c9c6ae54adc997cbd42453d82686e5b235aa4
Conrad Irwin <conrad.irwin@gmail.com>
2023-03-02 22:20:41 -0700

    Allow a local rebuild

--- deps/rebuild.sh
+++ deps/rebuild.sh
@@ -24,26 +24,33 @@ automerge_c="$deps/automerge-rs/rust/automerge-c"
 if [[ "$1" == "clean" ]]; then
     rm -rf "$deps/build"
 
+elif [[ "$1" == "local" ]]; then
+    true
+
 elif [[ "$1" != "" ]]; then
     echo "Unknown argument: $1"
     exit 1
 fi
 
 mkdir -p "$deps/build"
-
-CBINDGEN_TARGET_DIR="$deps/.." cargo build -r --manifest-path="$automerge_c/Cargo.toml" --target aarch64-apple-darwin --target-dir "$deps/build"
-cargo build -r --manifest-path="$automerge_c/Cargo.toml" --target x86_64-apple-darwin --target-dir "$deps/build"
-cross build -r --manifest-path="$automerge_c/Cargo.toml" --target aarch64-unknown-linux-gnu --target-dir "$deps/build"
-cross build -r --manifest-path="$automerge_c/Cargo.toml" --target x86_64-unknown-linux-gnu --target-dir "$deps/build"
-
-mkdir -p "$deps/include"
 mkdir -p "$deps/darwin_arm64"
 mkdir -p "$deps/darwin_amd64"
 mkdir -p "$deps/linux_arm64"
 mkdir -p "$deps/linux_amd64"
 
-cp "$deps/build/include/automerge-c/automerge.h" "$deps/../"
+CBINDGEN_TARGET_DIR="$deps/.." cargo build -r --manifest-path="$automerge_c/Cargo.toml" --target aarch64-apple-darwin --target-dir "$deps/build"
 cp "$deps/build/aarch64-apple-darwin/release/libautomerge_core.a" "$deps/darwin_arm64/"
+
+if [[ "$1" == "local" ]]; then
+    exit
+fi
+
+cargo build -r --manifest-path="$automerge_c/Cargo.toml" --target x86_64-apple-darwin --target-dir "$deps/build"
 cp "$deps/build/x86_64-apple-darwin/release/libautomerge_core.a" "$deps/darwin_amd64/"
+
+cross build -r --manifest-path="$automerge_c/Cargo.toml" --target aarch64-unknown-linux-gnu --target-dir "$deps/build"
 cp "$deps/build/aarch64-unknown-linux-gnu/release/libautomerge_core.a" "$deps/linux_arm64/"
+
+cross build -r --manifest-path="$automerge_c/Cargo.toml" --target x86_64-unknown-linux-gnu --target-dir "$deps/build"
 cp "$deps/build/x86_64-unknown-linux-gnu/release/libautomerge_core.a" "$deps/linux_amd64/"
+
